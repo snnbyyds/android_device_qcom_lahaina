d72902142e3ac7c9e72a87b31fa27cc0f00a0ff2
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    lahaina: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- device.mk
+++ device.mk
@@ -400,10 +400,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
-# QTI service tracker
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.servicetracker@1.2.vendor
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
