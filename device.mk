c399b46ef5a2501782cbb23c42c42cd214a41485
ReallySnow <reallysnow233@gmail.com>
2023-11-13 16:22:53 +0800

    lahaina: overlay: Configure status bar height and rounded corners
    Change-Id: I0d62310ec1ebc0950c03af462855e954955acb58

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
+    LahainaFrameworks \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon \
