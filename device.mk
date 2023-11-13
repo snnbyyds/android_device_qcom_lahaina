1571e89b2fbbee7096019a19ed06c55e28fafd3a
ReallySnow <reallysnow233@gmail.com>
2023-11-13 18:03:39 +0000

    lahaina: overlay: Enable smooth display
    Change-Id: I61020ac48aad8fc3c38f938139fd71cfb8f5b2fc

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
     LahainaFrameworks \
+    LahainaSettings \
     LahainaSystemUI \
     SystemUIResCommon \
     TelephonyResCommon \
