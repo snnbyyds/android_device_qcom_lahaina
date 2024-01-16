b223e5f58a638108174f9229cfc51e16c1d9743e
snnbyyds <snnbyyds@gmail.com>
2024-01-16 18:22:39 +0800

    lahaina: Build bootctl
    Change-Id: Ie47a2852daa6850845cb25159aeb793f643e2d6a

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
