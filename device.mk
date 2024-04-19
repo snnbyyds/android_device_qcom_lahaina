819206c9378db384f18a1f0af73e68b9bc9dfcc2
snnbyyds <snnbyyds@gmail.com>
2024-04-19 15:43:31 +0800

    lahaina: Drop dead OMX target
    Change-Id: I32994495d463751809d3875301cd206b81e21774

--- device.mk
+++ device.mk
@@ -354,7 +354,6 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    android.hardware.media.omx@1.0-impl \
     android.hardware.media.omx@1.0-service \
     libOmxAacEnc \
     libOmxAmrEnc \
